7c59bbf7ab713f457246732412b110ed92384161
Patrick Li <like.ca@gmail.com>
2019-08-12 15:04:57 -0400

    update var library_name

--- Library/convert_library.py
+++ Library/convert_library.py
@@ -1,31 +1,32 @@
 import csv, json
 
 
-def convert_library():
+def convert_library(library_name):
     csv_read = ''
     write_buffer = []
-    with open('library_b.csv', encoding='utf8') as rfile:
+    with open(library_name + '.csv', encoding='utf8') as rfile:
         csv_reader = csv.reader(rfile, delimiter=',')
         for row in csv_reader:
-            print(row[0].split('，'))
-            # print(row[1])
+            # print(row[0].split('，'))
+            print(row[1])
             write_buffer.append(row[1].strip())
 
     print(write_buffer)
     print(len(write_buffer))
 
-    with open('library_b.json', 'w', encoding='utf8') as wfile:
+    with open(library_name + '.json', 'w', encoding='utf8') as wfile:
         json.dump(write_buffer, wfile)
 
-def verify_library():
-    with open('library_b.json', encoding='utf8') as rfile:
+def verify_library(library_name):
+    with open(library_name + '.json', encoding='utf8') as rfile:
         data = json.load(rfile)
-        print(data[0])
-        print(data[299])
+        # print(data[0])
+        # print(data[299])
         print(len(data))
 
 
 
 if __name__ == "__main__":
-    convert_library()
-    verify_library()
+    library_name = 'library_b'
+    convert_library(library_name)
+    verify_library(library_name)
